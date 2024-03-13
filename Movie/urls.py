eb768d7e065cae3c55cbd6e13cb7312fa54d55ed
Bora Eryazgan <92815050+boraeryazgan@users.noreply.github.com>
2024-03-13 13:31:59 +0300

    changes

--- Movie/urls.py
+++ Movie/urls.py
@@ -3,7 +3,7 @@ from .views import login_view, signup, logout_view,index,detail,search
 
 urlpatterns = [
     path('', index, name='index'),
-    path('search/', search, name='search'),  # Arama işlevi için URL yönlendirmesi
+    path('search/', search, name='search'), 
 
     path('login/', login_view, name='login'),
     path('signup/', signup, name='signup'),
